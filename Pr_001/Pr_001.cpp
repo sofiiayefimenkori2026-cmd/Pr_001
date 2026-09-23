9bc49ada0a0e40b8f6d1a4b2243d86c0e3681666
unknown <sofiia.yefimenko.ri.2026@lpnu.ua>
2026-09-24 00:34:46 +0300

    first commit

---
+++ Pr_001/Pr_001.cpp
@@ -0,0 +1,28 @@
+﻿//Lab_02.cpp
+//Єфіменко Софія Володимирівна
+//Лабораторна робота №2
+//Варіант 8
+#include <iostream>
+#include <cmath>
+#include <Windows.h>
+using namespace std;
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	double x, y;
+	cout << "Введіть x: ";
+	cin >> x;
+	cout << "Введіть y: ";
+	cin >> y;
+	//Обчислення виразів
+	double z1 = pow(cos(x), 4) + pow(sin(y), 2) + 0.25 * pow(sin(2 * x), 2) - 1;
+	double z2 = sin(y + x) * sin(y - x);
+	//Виведення результатів
+	cout << endl;
+		cout << " z1 = " << z1 << endl;
+		cout << " z2 = " << z2 << endl;
+		//Щоб вікно у консолі не закривалося
+		cin.get();
+			return 0;
+}
